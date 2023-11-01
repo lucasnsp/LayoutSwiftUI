a756146e98adf28bd0be1a18324d9014d025fb9a
Lucas Neves <lucas.neves12@icloud.com>
2023-11-01 13:14:08 -0300

    Learning the diferences between VStack, ZStack and HStack.

--- LayoutSwiftUI/LayoutSwiftUI/ContentView.swift
+++ LayoutSwiftUI/LayoutSwiftUI/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10.0) {
+            Text("Meu primeiro Elemento")
+            Text("Meu segundo Elemento")
+            Text("Meu terceiro Elemento")
         }
-        .padding()
+//        HStack {
+//            Image(systemName: "circle.fill")
+//            Image(systemName: "square.fill")
+//            Image(systemName: "triangle.fill")
+//        }
+//        ZStack {
+//            Rectangle()
+//                .fill(Color.blue)
+//                .frame(width: 100, height: 100)
+//            Circle()
+//                .fill(Color.red)
+//                .frame(width: 100, height: 100)
+//            Text("ZStack")
+//                .bold()
+//        }
     }
 }
 
