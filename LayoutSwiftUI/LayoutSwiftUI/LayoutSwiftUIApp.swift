ea32a1b64994d1df4da9ed7fca4b25bcd251bd73
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-01 12:54:27 -0300

    Add files via upload

---
+++ LayoutSwiftUI/LayoutSwiftUI/LayoutSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutSwiftUIApp.swift
+//  LayoutSwiftUI
+//
+//  Created by Lucas Neves dos santos pompeu on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
